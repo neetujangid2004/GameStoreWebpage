735588dd1fec1da12274e2c82e65b147b7899aaf
neetujangid2004 <neetujangid166@gmail.com>
2025-11-04 13:32:51 +0530

    add links

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -1,10 +1,19 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import "./header.css";
 import userImg from "../images/user.jpg";
 import { AppContext } from "../App";
 
 function Header({ toggleActive }) {
   const { library, bag } = useContext(AppContext);
+  const [userName, setUserName] = useState("User Name");
+
+  // function to handle avatar click
+  const handleAvatarClick = () => {
+    const newName = prompt("Enter your username:");
+    if (newName && newName.trim() !== "") {
+      setUserName(newName.trim());
+    }
+  };
 
   return (
     <header>
@@ -20,12 +29,16 @@ function Header({ toggleActive }) {
           <i className="bi bi-bag-fill"></i>
           <span className="bag">{bag.length}</span>
         </a>
-        <div className="avatar">
+        <div
+          className="avatar"
+          onClick={handleAvatarClick}
+          style={{ cursor: "pointer" }}
+        >
           <a href="#">
             <img src={userImg} alt="User Image" />
           </a>
           <div className="user">
-            <span>User Name</span>
+            <span>{userName}</span>
             <a href="#">View Profile</a>
           </div>
         </div>
